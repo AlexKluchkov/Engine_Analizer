fd706c47572a385df0c9792739acde449390f1b6
Alexander Kluchkov <136575524+AlexKluchkov@users.noreply.github.com>
2025-01-27 07:48:13 +0200

    Update Spectrogram_Analysis.py

--- Spectrogram_Analysis.py
+++ Spectrogram_Analysis.py
@@ -4,6 +4,8 @@ import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 
+import Console
+
 class Spectrogram_Analysis():
 
     def normalize_spectrogram(spectrogram, min_val=0, max_val=1):
@@ -13,29 +15,28 @@ class Spectrogram_Analysis():
         normalized_spectrogram = normalized_spectrogram * (max_val - min_val) + min_val
         return normalized_spectrogram
 
+
     def cosinus_compare_spectrgrum(self, y1, y2, sr1, sr2):
 
         if len(y1) > len(y2):
-            y1 = y1[:len(y2)]
+            y1 = y1[:len(y2)]  # Обрезать
         elif len(y1) < len(y2):
             y2 = y2[:len(y1)]
-        
+
+        # Преобразование в спектрограммы
         S1 = librosa.stft(y1)
         S2 = librosa.stft(y2)
-        
+        # Амплитудный спектр
         S1_db = librosa.amplitude_to_db(np.abs(S1), ref=np.max)
         S2_db = librosa.amplitude_to_db(np.abs(S2), ref=np.max)
 
         S1_db = Spectrogram_Analysis.normalize_spectrogram(S1_db)
         S2_db = Spectrogram_Analysis.normalize_spectrogram(S2_db)
 
+        # Перетвореня спектрограм у вектори
         vec1 = S1_db.flatten()
         vec2 = S2_db.flatten()
 
         cos_sim = 1 - cosine(vec1, vec2)
-        
-        C = Console.Console()
-        C.GetAnswer(f"Cosine Similarity: {cos_sim}")
-        C.show()
-        return C
 
+        return cos_sim
